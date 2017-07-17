3fb30a8c694fa7fe8c8252c68f62070f628e8a48
hbanyal <hbanyal@connecture.com>
2017-07-17 13:57:00 +0530

    Update Invertebrate.java

--- src/main/java/com/hitendra/Invertebrate.java
+++ src/main/java/com/hitendra/Invertebrate.java
@@ -1,6 +1,6 @@
 package com.hitendra;
 
-public abstract class Invertebrate {
+public abstract class Invertebrate  extends Animal{
 	protected boolean hasBackbone = false;
 
 	public boolean hasBackbone() {
